651484a5f3e37b90798d7a3d5e6efeff16a3e246
Ramazan Nazmiev <92743390+blxxdclxud@users.noreply.github.com>
2024-07-22 03:44:13 +0300

    Delete src/api directory

--- src/api/logout.ts
+++
@@ -1,9 +0,0 @@
-// import { NextRequest } from 'next/server';
-// import Cookies from 'js-cookie';
-//
-// function Logout(req: NextRequest): void {
-// 	req.cookies.clear();
-//
-// }
-//
-// export default Logout;
